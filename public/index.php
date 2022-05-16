e46ebbc335f938832cd8758627a9953e16a99bfe
Luutshi <luutshi@gmail.com>
2022-05-16 14:38:10 +0200

    adding backoffice location side

--- public/index.php
+++ public/index.php
@@ -10,5 +10,8 @@ $router->get('/', 'Mvc\Controllers\PageController@base');
 $router->all('/register', 'Mvc\Controllers\UserController@register');
 $router->all('/login', 'Mvc\Controllers\UserController@login');
 $router->get('/logout', 'Mvc\Controllers\UserController@logout');
+$router->get('/admin/location/delete/{id}', 'Mvc\Controllers\PageController@locationDelete');
+$router->all('/admin/location/create', 'Mvc\Controllers\PageController@locationCreate');
+$router->get('/admin/location', 'Mvc\Controllers\PageController@locationList');
 
 $router->run();
